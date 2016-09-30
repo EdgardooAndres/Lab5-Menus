e4f20c4c27632e1c27e22741f5f62fe390993cb6
EggieMetal <edgardo.muniz@upr.edu>
2016-09-30 13:28:48 -0400

    learn

--- Lab5 Menus/src/menuClasses/AlterContentsOfListMenu.java
+++ Lab5 Menus/src/menuClasses/AlterContentsOfListMenu.java
@@ -1,11 +1,27 @@
 package menuClasses;
 
-public class AlterContentsOfListMenu implements Action {
+import java.util.ArrayList;
 
-	@Override
-	public void execute(Object args) {
-		// TODO Auto-generated method stub
+public class AlterContentsOfListMenu extends Menu {
+	private static AlterContentsOfListMenu ACM = new AlterContentsOfListMenu(); 
+	private AlterContentsOfListMenu() { 
+		super(); 
+		String title; 
+		ArrayList<Option> options = new ArrayList<Option>();  
+		title = "Statistics on Lists"; 
+		options.add(new Option("Show all Lists", new ShowListsAction())); 
+		options.add(new Option("Show Content of a List", new ShowListAction())); 
+		options.add(new Option("Show sum of a List", new ShowListSumAction())); 
+		options.add(new Option("Show average of a List", new ShowListAverageAction())); 
+		options.add(new Option("Show max value in a List", new ShowMaxValueListAction())); 
+		options.add(Option.EXIT); 
 
+		super.InitializeMenu(title, options); 
+
+	}
+	
+	public static AlterContentsOfListMenu getListStatisticsMenu() { 
+		return ACM; 
 	}
 
 }
--- Lab5 Menus/src/menuClasses/MainMenu.java
+++ Lab5 Menus/src/menuClasses/MainMenu.java
@@ -11,8 +11,9 @@ public class MainMenu extends Menu {
 		title = "Main Menu"; 
 		options.add(new Option("Show all Lists", new ShowListsAction())); 
 		options.add(new Option("Create New List", new CreateListAction())); 
-		options.add(new Option("Delete an Existing List", new DeleteFromListAction()));
+		options.add(new Option("Delete an Existing List", new DeleteListAction()));
 		options.add(new Option("Operations on Lists", new OperateListAction())); 
+		options.add(new Option("Alter Content of a List", new OperateListAction())); 
 		options.add(Option.EXIT); 
 
 		super.InitializeMenu(title, options);
